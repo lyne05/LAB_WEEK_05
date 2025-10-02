891f5ccec193616bfcde6b945a10d9e374592d8b
lyne05 <evelyne.natalie2705@gmail.com>
2025-10-02 14:31:53 +0700

    Commit No. 3 - Load and Display Cat Image using Glide

--- app/src/main/java/com/example/lab_week_05/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_05/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.lab_week_05
 
+import android.widget.ImageView
 import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
@@ -11,22 +12,34 @@ import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import com.example.lab_week_05.api.CatApiService
 import com.example.lab_week_05.model.ImageData
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 
 class MainActivity : AppCompatActivity() {
 
     private val retrofit by lazy {
+        val moshi = Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+
         Retrofit.Builder()
             .baseUrl("https://api.thecatapi.com/v1/")
-            .addConverterFactory(MoshiConverterFactory.create())
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
             .build()
     }
 
-    private val catApiService by lazy {
+    private val catApiService by lazy{
         retrofit.create(CatApiService::class.java)
     }
-
-    private val apiResponseView: TextView by lazy {
+    private val apiResponseView: TextView by lazy{
         findViewById(R.id.api_response)
+    }
+    private val imageResultView: ImageView by lazy {
+        findViewById(R.id.image_result)
+    }
+
+    private val imageLoader: ImageLoader by lazy {
+        GlideLoader(this)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,23 +50,26 @@ class MainActivity : AppCompatActivity() {
 
     private fun getCatImageResponse() {
         val call = catApiService.searchImages(1, "full")
-        call.enqueue(object : Callback<List<ImageData>> {
+        call.enqueue(object: Callback<List<ImageData>> {
             override fun onFailure(call: Call<List<ImageData>>, t: Throwable) {
                 Log.e(MAIN_ACTIVITY, "Failed to get response", t)
             }
-
-            override fun onResponse(
-                call: Call<List<ImageData>>,
-                response: Response<List<ImageData>>
-            ) {
-                if (response.isSuccessful) {
-                    val imageList = response.body()
-                    val firstImage = imageList?.firstOrNull()?.imageUrl ?: "No URL"
-                    apiResponseView.text = getString(R.string.image_placeholder, firstImage)
-                } else {
-                    Log.e(
-                        MAIN_ACTIVITY,
-                        "Failed to get response\n${response.errorBody()?.string().orEmpty()}"
+            override fun onResponse(call: Call<List<ImageData>>, response:
+            Response<List<ImageData>>) {
+                if(response.isSuccessful){
+                    val image = response.body()
+                    val firstImage = image?.firstOrNull()?.url.orEmpty()
+                    if (firstImage.isNotBlank()) {
+                        imageLoader.loadImage(firstImage, imageResultView)
+                    } else {
+                        Log.d(MAIN_ACTIVITY, "Missing image URL")
+                    }
+                    apiResponseView.text = getString(R.string.image_placeholder,
+                        firstImage)
+                }
+                else{
+                    Log.e(MAIN_ACTIVITY, "Failed to get response\n" +
+                            response.errorBody()?.string().orEmpty()
                     )
                 }
             }
--- app/src/main/java/com/example/lab_week_05/model/CatBreedData.kt
+++ app/src/main/java/com/example/lab_week_05/model/CatBreedData.kt
@@ -1,6 +1,7 @@
 package com.example.lab_week_05.model
-
+import com.squareup.moshi.JsonClass
+@JsonClass(generateAdapter = true)
 data class CatBreedData(
-    val name: String,
-    val temperament: String
+    val name: String?,
+    val temperament: String?
 )
