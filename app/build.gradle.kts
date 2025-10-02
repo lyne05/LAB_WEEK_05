e4155339c0b2b21dd83f7d7823e14faffd86fe26
lyne05 <evelyne.natalie2705@gmail.com>
2025-10-02 09:25:06 +0700

    Commit No. 1 - Retrofit Setup and Fetch API JSON Response

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,7 +36,8 @@ android {
 }
 
 dependencies {
-
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.converter.scalars)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
